3362bf174fe95b04a7de71f5978e1e3867e946d3
ig_2014 <ig_2014@igtp4-mac04.local>
2014-02-20 10:11:05 +0100

    création base saine
    création empty project pour une base de travail saine

---
+++ xcode_project/xcode_project/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  xcode_project
+//
+//  Created by ig_2014 on 20/02/2014.
+//  Copyright (c) 2014 ig_2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ xcode_project/xcode_projectTests/xcode_projectTests.m
@@ -0,0 +1,34 @@
+//
+//  xcode_projectTests.m
+//  xcode_projectTests
+//
+//  Created by ig_2014 on 20/02/2014.
+//  Copyright (c) 2014 ig_2014. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface xcode_projectTests : XCTestCase
+
+@end
+
+@implementation xcode_projectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
